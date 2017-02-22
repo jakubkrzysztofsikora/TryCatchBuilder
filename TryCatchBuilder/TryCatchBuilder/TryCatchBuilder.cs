d272283c285c41003b9cbeada9c2c22ed78d9b75
Jakub Sikora <ksikora@future-processing.com>
2017-02-22 09:53:27 +0000

    Added ready library with unit tests (97% coverage) - extracted from one of the projects with little modifications

---
+++ TryCatchBuilder/TryCatchBuilder/TryCatchBuilder.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace TryCatchBuilder
+{
+    public class TryCatchBuilder<TReturnFromWrappedFunction>
+    {
+		private static readonly Action DoActionNothing = () => { };
+
+		private readonly Func<TReturnFromWrappedFunction> _function;
+		private readonly Dictionary<Type, Action<Exception>> _catchCollection;
+		private int _retryCount;
+		private TimeSpan _retryInterval;
+		private TimeSpan _retryIncrement;
+		private Action _onReTryAction;
+		private Action _onRepeatedFailureAction;
+
+		public static TryCatchBuilder<TReturnFromWrappedFunction> Try(Func<TReturnFromWrappedFunction> function)
+		{
+			return new TryCatchBuilder<TReturnFromWrappedFunction>(function);
+		}
+
+		protected TryCatchBuilder(Func<TReturnFromWrappedFunction> function)
+		{
+			_function = function;
+			_catchCollection = new Dictionary<Type, Action<Exception>>();
+			_retryCount = 0;
+			_retryInterval = TimeSpan.Zero;
+			_retryIncrement = TimeSpan.Zero;
+			_onReTryAction = DoActionNothing;
+			_onRepeatedFailureAction = DoActionNothing;
+		}
+
+
+		public TryCatchBuilder<TReturnFromWrappedFunction> Catch<TException>(Action<Exception> onExceptionCatched)
+			where TException : Exception
+		{
+			_catchCollection.Add(typeof(TException), onExceptionCatched);
+			return this;
+		}
+
+		public Func<TReturnFromWrappedFunction> Build()
+		{
+			return () =>
+			{
+				for (int i = 0; i <= _retryCount; ++i, Thread.Sleep(_retryInterval), _retryInterval += _retryIncrement, _onReTryAction())
+				{
+					try
+					{
+						return _function();
+					}
+					catch (Exception e)
+					{
+						foreach (var catchElement in _catchCollection)
+						{
+							if (e.GetType() == catchElement.Key)
+							{
+								catchElement.Value(e);
+							}
+						}
+					}
+				}
+
+				_onRepeatedFailureAction();
+
+				return default(TReturnFromWrappedFunction);
+			};
+		}
+
+		public TryCatchBuilder<TReturnFromWrappedFunction> ReTry(int retryCount, TimeSpan baseRetryInterval, TimeSpan? retryIncrement = null, Action onReTryAction = null)
+		{
+			_retryCount = retryCount < 0 ? 0 : retryCount;
+			_retryInterval = baseRetryInterval;
+			_retryIncrement = retryIncrement ?? TimeSpan.Zero;
+			_onReTryAction = onReTryAction ?? DoActionNothing;
+			return this;
+		}
+
+		public TryCatchBuilder<TReturnFromWrappedFunction> OnRepeatedFailure(Action onRepeatedFailureAction)
+		{
+			_onRepeatedFailureAction = onRepeatedFailureAction ?? DoActionNothing;
+			return this;
+		}
+	}
+}
---
+++ TryCatchBuilder/TryCatchBuilderTests/UnitTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using TryCatchBuilder;
+
+namespace TryCatchBuilderTests
+{
+	[TestFixture]
+	public class UnitTests
+	{
+		private int _throwedExceptions;
+
+		[TestCase(true)]
+		[TestCase(false)]
+		public void
+			GivenFunctionToPerformAndActionToDoOnFail_WhenWrappingSimpleFunctionWithCatchAndExecuting_ShouldPerformActionGivenToCatchIfFailedAndNotIfNoExceptionThrown(bool shouldThrowException)
+		{
+			//Given
+			Func<int> functionToWrap = () => FunctionToPerformByTryCatchBuilder(shouldThrowException);
+			bool hasOnFailActionPerformed = false;
+			Action<Exception> actionOnFail = exception => hasOnFailActionPerformed = true;
+
+			//When
+			Func<int> wrappedFunction = TryCatchBuilder<int>
+				.Try(functionToWrap)
+				.Catch<Exception>(actionOnFail)
+				.Build();
+			wrappedFunction();
+
+			//Then
+			Assert.That(hasOnFailActionPerformed, Is.EqualTo(shouldThrowException));
+		}
+
+		[Test]
+		public void
+			GivenRepeatedlyFailingFunctionToPerformAndActionToPerformOnRepeatedFailure_WhenWrappingSimpleFunctionWithCatchAndExecuting_ShouldPerformFunctionAFewTimesBeforeExecutingActionOnRepeatedFailure
+			()
+		{
+			//Given
+			Func<int> functionToWrap = () => FunctionToPerformByTryCatchBuilder(true);
+			bool hasOnRepeatedFailActionPerformed = false;
+			Action actionOnRepeatedFail = () => hasOnRepeatedFailActionPerformed = true;
+			int numberOfTries = 4;
+			int numberofRetries = numberOfTries - 1;
+			_throwedExceptions = 0;
+
+			//When
+			Func<int> wrappedFunction = TryCatchBuilder<int>
+				.Try(functionToWrap)
+				.Catch<Exception>(JustCountTheRetries)
+				.ReTry(numberofRetries, TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(1))
+				.OnRepeatedFailure(actionOnRepeatedFail)
+				.Build();
+			wrappedFunction();
+
+			//Then
+			Assert.That(hasOnRepeatedFailActionPerformed, Is.True);
+			Assert.That(_throwedExceptions, Is.EqualTo(numberOfTries));
+		}
+
+		[Test]
+		public void
+			GivenFunctionAFewTimesFailingButFinallySucceeding_WhenWrappingSimpleFunctionFunctionWithCatchAndExecuting_ShouldNotPerformRepeatedFailureAction
+			()
+		{
+			//Given
+			int numberOfFailedTries = 2;
+			int numberOfTries = 4;
+			int numberofRetries = numberOfTries - 1;
+			Func<int> functionToWrap = () =>
+			{
+				if (_throwedExceptions < numberOfFailedTries)
+				{
+					throw new Exception();
+				}
+
+				return 1;
+			};
+			bool hasOnRepeatedFailActionPerformed = false;
+			Action actionOnRepeatedFail = () => hasOnRepeatedFailActionPerformed = true;
+			_throwedExceptions = 0;
+
+			//When
+			Func<int> wrappedFunction = TryCatchBuilder<int>
+				.Try(functionToWrap)
+				.Catch<Exception>(JustCountTheRetries)
+				.ReTry(numberofRetries, TimeSpan.FromMilliseconds(50), TimeSpan.FromMilliseconds(1))
+				.OnRepeatedFailure(actionOnRepeatedFail)
+				.Build();
+			wrappedFunction();
+
+			//Then
+			Assert.That(hasOnRepeatedFailActionPerformed, Is.False);
+			Assert.That(_throwedExceptions, Is.EqualTo(numberOfFailedTries));
+		}
+
+		private int FunctionToPerformByTryCatchBuilder(bool shouldThrowException)
+		{
+			if (shouldThrowException)
+				throw new Exception("Just executing the orders here :)");
+
+			return 1;
+		}
+
+		private void JustCountTheRetries(Exception exception)
+		{
+			++_throwedExceptions;
+		}
+	}
+}
